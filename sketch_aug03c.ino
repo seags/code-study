ae2e676eb72ec1e3da4b6ac08036bab3ed46989b
Noothoot <69259460+Noothoot@users.noreply.github.com>
2020-08-05 14:35:33 -0400

    Update sketch_aug03c.ino

--- sketch_aug03c.ino
+++ sketch_aug03c.ino
@@ -1,41 +1,27 @@
-/* 
- Educational BoosterPack MK II - Birthday Tune
-http://boosterpackdepot.info/wiki/index.php?title=Educational_BoosterPack_MK_II
- 
- Play birthday tune through the buzzer, demonstrating
- buzzer tune() API and pitch/tone (hence music) generation
- 
- Dec 2012 - Created for Educational BoosterPack
-            buzzer Pin = 19
- Dec 2013 - Modified for Educational BoosterPack MK II
-            buzzer Pin = 40
- by Dung Dang
- 
- */
 
-#define NOTE_C4_1 260
-
-int buzzerPin = 40;
-int buttonPin = 32;
-// notes in the melody:
-
-// note durations: 4 = quarter note, 8 = eighth note, etc.:
-
-
+const int buzzerPin = 40;
+const int buttonPin = 32;
+int buttonState = HIGH;
 void setup() 
 {
-pinMode(buzzerPin,OUTPUT);
+pinMode(buttonPin,INPUT);
+pinMode(buzzerPin, OUTPUT);
+Serial.begin(9600);
+
 }
 void loop() 
 {
-
- 
-;
+while(buttonState == HIGH){
+  
+ buttonState = digitalRead(buttonPin);
+   
+    
     tone(40, 277, 1000);
 
       //delay between pulse
-    delay(50);
+    delay(1300);
     
-    noTone(buzzerPin);                // stop the tone playing
-  }
+    noTone(buzzerPin);   
+    buttonState = digitalRead(buttonPin);
+}
 }
